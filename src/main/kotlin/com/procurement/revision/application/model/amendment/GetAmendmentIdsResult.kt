ec48f214420f1dc8054af82e4abd93ec12b1e1c9
katherine <riodael@gmail.com>
2020-02-10 14:11:21 +0200

    ES-2516 Added GetAmendmentIds response and result

---
+++ src/main/kotlin/com/procurement/revision/application/model/amendment/GetAmendmentIdsResult.kt
@@ -0,0 +1,7 @@
+package com.procurement.revision.application.model.amendment
+
+import com.procurement.revision.domain.model.amendment.AmendmentId
+
+data class GetAmendmentIdsResult(
+    val id: AmendmentId
+)
---
+++ src/main/kotlin/com/procurement/revision/infrastructure/web/dto/response/amendment/GetAmendmentIdsResponse.kt
@@ -0,0 +1,5 @@
+package com.procurement.revision.infrastructure.web.dto.response.amendment
+
+import com.procurement.revision.domain.model.amendment.AmendmentId
+
+class GetAmendmentIdsResponse(val id: AmendmentId)

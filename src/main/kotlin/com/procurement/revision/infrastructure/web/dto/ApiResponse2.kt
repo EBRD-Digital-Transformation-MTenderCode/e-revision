75ee89182a7f9264fba6d8dfcf1422c135b3a7c3
katherine <riodael@gmail.com>
2020-02-11 16:48:27 +0200

    ES-2516 ApiResponse2 now has heirs for success, fail and incident

--- src/main/kotlin/com/procurement/revision/infrastructure/web/dto/ApiResponse2.kt
+++ src/main/kotlin/com/procurement/revision/infrastructure/web/dto/ApiResponse2.kt
@@ -3,21 +3,54 @@ package com.procurement.revision.infrastructure.web.dto
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonValue
+import java.time.LocalDateTime
 import java.util.*
 
-class ApiResponse2(
+sealed class ApiResponse2(
     @field:JsonProperty("version") @param:JsonProperty("version") val version: ApiVersion,
     @field:JsonProperty("id") @param:JsonProperty("id") val id: UUID,
-    @field:JsonProperty("status") @param:JsonProperty("status") val status: ResponseStatus,
-
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     @field:JsonProperty("result") @param:JsonProperty("result") val result: Any?
-)
+) {
+    abstract val status: ResponseStatus
+}
+
+class ApiSuccessResponse2(
+    version: ApiVersion, id: UUID,
+    @JsonInclude(JsonInclude.Include.NON_EMPTY) result: Any?
+) : ApiResponse2(
+    version = version,
+    id = id,
+    result = result
+) {
+    @field:JsonProperty("status")
+    override val status: ResponseStatus = ResponseStatus.SUCCESS
+}
+
+class ApiFailResponse2(version: ApiVersion, id: UUID, result: List<Error>) :
+    ApiResponse2(version = version, id = id, result = result) {
+
+    @field:JsonProperty("status")
+    override val status: ResponseStatus = ResponseStatus.FAIL
+
+    class Error(val code: String, val description: String?)
+}
+
+class ApiIncidentResponse2(version: ApiVersion, id: UUID, result: Incident) :
+    ApiResponse2(version = version, id = id, result = result) {
+
+    @field:JsonProperty("status")
+    override val status: ResponseStatus = ResponseStatus.INCIDENT
+
+    class Incident(val id: UUID, date: LocalDateTime, val service: Service, val errors: List<Error>) {
+        class Service(val id: String, name: String, version: ApiVersion)
+        class Error(val code: String, val description: String, val metadata: Any?)
+    }
+}
 
 enum class ResponseStatus(private val value: String) {
 
     SUCCESS("success"),
-    FAILURE("failure"),
+    FAIL("fail"),
     INCIDENT("incident");
 
     @JsonValue
@@ -29,8 +62,3 @@ enum class ResponseStatus(private val value: String) {
         return this.value
     }
 }
-
-class ApiErrorResult2(
-    @field:JsonProperty("id") @param:JsonProperty("id") val id: String,
-    @field:JsonProperty("details") @param:JsonProperty("details") val details: String
-)

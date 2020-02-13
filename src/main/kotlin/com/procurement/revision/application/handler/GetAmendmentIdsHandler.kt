c3f3301d9761e8f6aea7c43ed56f440d8b285131
katherine <riodael@gmail.com>
2020-02-13 15:43:50 +0200

    ES-2516 GetAmendmentIdsHandler changed to work without AmendmentFilters

--- src/main/kotlin/com/procurement/revision/application/handler/GetAmendmentIdsHandler.kt
+++ src/main/kotlin/com/procurement/revision/application/handler/GetAmendmentIdsHandler.kt
@@ -4,42 +4,30 @@ import com.procurement.revision.application.model.amendment.GetAmendmentIdsData
 import com.procurement.revision.application.model.amendment.GetAmendmentIdsResult
 import com.procurement.revision.application.repository.AmendmentRepository
 import com.procurement.revision.domain.model.amendment.Amendment
-import com.procurement.revision.domain.model.amendment.AmendmentFilter
+import com.procurement.revision.domain.model.amendment.AmendmentId
 import org.springframework.stereotype.Component
 import java.util.function.Predicate
 
 @Component
 class GetAmendmentIdsHandler(private val amendmentRepository: AmendmentRepository) {
 
-    fun handle(data: GetAmendmentIdsData): List<GetAmendmentIdsResult> {
+    fun handle(data: GetAmendmentIdsData): List<AmendmentId> {
         val amendments = amendmentRepository.findBy(data.cpid)
-
-        val isAmendmentSuitable = data.relatedItems
-            .takeIf { it.isNotEmpty() }
-            ?.asSequence()
-            ?.map { item ->
-                Predicate<Amendment> { amendment ->
-                    AmendmentFilter(
-                        status = data.status,
-                        relatesTo = data.relatesTo,
-                        type = data.type,
-                        relatedItem = item
-                    ).compareWith(amendment)
-                }
-            }
-            ?.reduce { accumulatedPredicate, predicate -> accumulatedPredicate.or(predicate) }
-            ?: Predicate { amendment ->
-                AmendmentFilter(
-                    status = data.status,
-                    relatesTo = data.relatesTo,
-                    type = data.type
-                ).compareWith(amendment)
-            }
+        val relatedItems = data.relatedItems.toSet()
 
         return amendments
             .asSequence()
-            .filter { amendment -> isAmendmentSuitable.test(amendment) }
-            .map { amendment -> GetAmendmentIdsResult(amendment.id) }
+            .filter { amendment ->
+                testEquals(amendment.status, pattern = data.status)
+                    && testEquals(amendment.type, pattern = data.type)
+                    && testEquals(amendment.relatesTo, pattern = data.relatesTo)
+                    && testContains(amendment.relatedItem, patterns = relatedItems)
+            }
+            .map { amendment -> amendment.id }
             .toList()
     }
+
+    private fun <T> testEquals(value: T, pattern: T?): Boolean = if (pattern != null) value == pattern else true
+    private fun <T> testContains(value: T, patterns: Set<T>): Boolean =
+        if (patterns.isNotEmpty()) value in patterns else true
 }

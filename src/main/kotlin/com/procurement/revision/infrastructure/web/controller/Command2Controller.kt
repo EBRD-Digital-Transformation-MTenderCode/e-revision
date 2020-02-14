3d1f999188564fe4f2142cf05c9ec7fdcad57b42
katherine <riodael@gmail.com>
2020-02-14 18:46:09 +0200

    ES-2516 Rewrote controller to work with JsonNode instead of Command2Message

--- src/main/kotlin/com/procurement/revision/infrastructure/web/controller/Command2Controller.kt
+++ src/main/kotlin/com/procurement/revision/infrastructure/web/controller/Command2Controller.kt
@@ -3,10 +3,11 @@ package com.procurement.revision.infrastructure.web.controller
 import com.procurement.revision.infrastructure.configuration.properties.GlobalProperties
 import com.procurement.revision.infrastructure.service.Command2Service
 import com.procurement.revision.infrastructure.utils.toJson
-import com.procurement.revision.infrastructure.utils.toObject
+import com.procurement.revision.infrastructure.utils.toNode
 import com.procurement.revision.infrastructure.web.dto.ApiResponse2
-import com.procurement.revision.infrastructure.web.dto.Command2Message
+import com.procurement.revision.infrastructure.web.dto.ApiVersion
 import com.procurement.revision.infrastructure.web.dto.errorResponse2
+import com.procurement.revision.infrastructure.web.dto.getBy
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/command2")
@@ -28,8 +30,8 @@ class Command2Controller(private val commandService: Command2Service) {
         if (log.isDebugEnabled)
             log.debug("RECEIVED COMMAND: '$requestBody'.")
 
-        val cm: Command2Message = try {
-            requestBody.toObject(Command2Message::class.java)
+        val node = try {
+            requestBody.toNode()
         } catch (expected: Exception) {
             log.debug("Error.", expected)
             val response =
@@ -40,18 +42,40 @@ class Command2Controller(private val commandService: Command2Service) {
             return ResponseEntity(response, HttpStatus.OK)
         }
 
+        val id = try {
+            UUID.fromString(node.getBy("id").asText())
+        } catch (expected: Exception) {
+            log.debug("Error.", expected)
+            val response = errorResponse2(
+                exception = expected,
+                version = GlobalProperties.App.apiVersion
+            )
+            return ResponseEntity(response, HttpStatus.OK)
+        }
+        val version = try {
+            ApiVersion.valueOf(node.getBy("version").asText())
+        } catch (expected: Exception) {
+            log.debug("Error.", expected)
+            val response = errorResponse2(
+                id = id,
+                exception = expected,
+                version = GlobalProperties.App.apiVersion
+            )
+            return ResponseEntity(response, HttpStatus.OK)
+        }
+
         val response = try {
-            commandService.execute(cm)
+            commandService.execute(node)
                 .also { response ->
                     if (log.isDebugEnabled)
-                        log.debug("RESPONSE (id: '${cm.id}'): '${response.toJson()}'.")
+                        log.debug("RESPONSE (id: '${id}'): '${response.toJson()}'.")
                 }
         } catch (expected: Exception) {
             log.debug("Error.", expected)
             errorResponse2(
                 exception = expected,
-                id = cm.id,
-                version = cm.version
+                id = id,
+                version = version
             )
         }
         return ResponseEntity(response, HttpStatus.OK)

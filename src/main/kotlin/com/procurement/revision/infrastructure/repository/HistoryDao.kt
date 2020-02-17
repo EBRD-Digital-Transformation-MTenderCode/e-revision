e0d03ee00bc174e5b6eab8afbf4837d768a83327
katherine <riodael@gmail.com>
2020-02-17 16:20:09 +0200

    ES-2516 Added prepared statements to HistoryDao

--- src/main/kotlin/com/procurement/revision/infrastructure/repository/HistoryDao.kt
+++ src/main/kotlin/com/procurement/revision/infrastructure/repository/HistoryDao.kt
@@ -1,27 +1,54 @@
 package com.procurement.revision.infrastructure.repository
 
 import com.datastax.driver.core.Session
-import com.datastax.driver.core.querybuilder.QueryBuilder.eq
-import com.datastax.driver.core.querybuilder.QueryBuilder.insertInto
-import com.datastax.driver.core.querybuilder.QueryBuilder.select
 import com.procurement.revision.infrastructure.model.entity.HistoryEntity
 import com.procurement.revision.infrastructure.utils.localNowUTC
 import com.procurement.revision.infrastructure.utils.toDate
 import com.procurement.revision.infrastructure.utils.toJson
-import com.procurement.revision.infrastructure.web.dto.ApiResponse
-import com.procurement.revision.infrastructure.web.dto.ApiResponse2
 import org.springframework.stereotype.Service
 
 @Service
 class HistoryDao(private val session: Session) {
 
+    companion object {
+        private const val KEYSPACE = "revision"
+        private const val HISTORY_TABLE = "history"
+        private const val OPERATION_ID = "operation_id"
+        private const val COMMAND = "command"
+        private const val OPERATION_DATE = "operation_date"
+        private const val JSON_DATA = "json_data"
+
+        private const val SAVE_HISTORY_CQL = """
+               INSERT INTO $KEYSPACE.$HISTORY_TABLE(
+                      $OPERATION_ID,
+                      $COMMAND,
+                      $OPERATION_DATE
+                      $JSON_DATA
+               )
+               VALUES(?, ?, ?,?)
+               IF NOT EXISTS
+            """
+
+        private const val FIND_HISTORY_ENTRY_CQL = """
+               SELECT $OPERATION_ID,
+                      $COMMAND,
+                      $OPERATION_DATE
+                      $JSON_DATA
+                 FROM $KEYSPACE.$HISTORY_TABLE
+                WHERE $OPERATION_ID=?
+                  AND $COMMAND=?
+               LIMIT 1
+            """
+    }
+
+    private val preparedSaveHistoryCQL = session.prepare(SAVE_HISTORY_CQL)
+    private val preparedFindHistoryByCpidAndCommandCQL = session.prepare(FIND_HISTORY_ENTRY_CQL)
+
     fun getHistory(operationId: String, command: String): HistoryEntity? {
-        val query = select()
-            .all()
-            .from(HISTORY_TABLE)
-            .where(eq(OPERATION_ID, operationId))
-            .and(eq(COMMAND, command))
-            .limit(1)
+        val query = preparedFindHistoryByCpidAndCommandCQL.bind().apply {
+            setString(OPERATION_ID, operationId)
+            setString(COMMAND, command)
+        }
         val row = session.execute(query).one()
         return if (row != null) HistoryEntity(
             row.getString(OPERATION_ID),
@@ -39,20 +66,13 @@ class HistoryDao(private val session: Session) {
             jsonData = result.toJson()
         )
 
-        val insert = insertInto(HISTORY_TABLE)
-            .value(OPERATION_ID, entity.operationId)
-            .value(COMMAND, entity.command)
-            .value(OPERATION_DATE, entity.operationDate)
-            .value(JSON_DATA, entity.jsonData)
+        val insert = preparedSaveHistoryCQL.bind().apply {
+            setString(OPERATION_ID, entity.operationId)
+            setString(COMMAND, entity.command)
+            setTimestamp(OPERATION_DATE, entity.operationDate)
+            setString(JSON_DATA, entity.jsonData)
+        }
         session.execute(insert)
         return entity
-    }
-
-    companion object {
-        private const val HISTORY_TABLE = "history"
-        private const val OPERATION_ID = "operation_id"
-        private const val COMMAND = "command"
-        private const val OPERATION_DATE = "operation_date"
-        private const val JSON_DATA = "json_data"
     }
 }

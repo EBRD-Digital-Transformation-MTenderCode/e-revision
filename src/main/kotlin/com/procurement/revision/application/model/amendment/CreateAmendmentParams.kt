adfe9a3140c3c6248e2f38fca89262e7603e863f
katherine <riodael@gmail.com>
2020-02-19 17:09:36 +0200

    ES-2521 Added CreateAmendment request and params

---
+++ src/main/kotlin/com/procurement/revision/application/model/amendment/CreateAmendmentParams.kt
@@ -0,0 +1,33 @@
+package com.procurement.revision.application.model.amendment
+
+import com.procurement.revision.domain.enums.DocumentType
+import com.procurement.revision.domain.model.Owner
+import com.procurement.revision.domain.model.amendment.AmendmentId
+import com.procurement.revision.domain.model.document.DocumentId
+import com.procurement.revision.infrastructure.model.OperationType
+import java.time.LocalDateTime
+import java.util.*
+
+class CreateAmendmentParams(
+     val amendment: Amendment,
+     val id: UUID,
+     val operationType: OperationType,
+     val startDate: LocalDateTime,
+     val cpid: String,
+     val ocid: String,
+     val owner: Owner
+) {
+    data class Amendment(
+         val rationale: String,
+         val description: String?,
+         val documents: List<Document>,
+         val id: AmendmentId
+    ) {
+        data class Document(
+            val documentType: DocumentType,
+            val id: DocumentId,
+            val title: String,
+            val description: String?
+        )
+    }
+}

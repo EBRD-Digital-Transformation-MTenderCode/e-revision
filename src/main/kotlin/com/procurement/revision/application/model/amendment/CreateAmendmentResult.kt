f74ba54281e320ecf67e3d0292712b15095fcee6
katherine <riodael@gmail.com>
2020-02-19 17:12:25 +0200

    ES-2521 Added CreateAmendment response and result

---
+++ src/main/kotlin/com/procurement/revision/application/model/amendment/CreateAmendmentResult.kt
@@ -0,0 +1,30 @@
+package com.procurement.revision.application.model.amendment
+
+import com.procurement.revision.domain.enums.AmendmentRelatesTo
+import com.procurement.revision.domain.enums.AmendmentStatus
+import com.procurement.revision.domain.enums.AmendmentType
+import com.procurement.revision.domain.enums.DocumentType
+import com.procurement.revision.domain.model.Token
+import com.procurement.revision.domain.model.amendment.AmendmentId
+import com.procurement.revision.domain.model.document.DocumentId
+import java.time.LocalDateTime
+
+data class CreateAmendmentResult(
+    val rationale: String,
+    val description: String?,
+    val documents: List<Document>,
+    val id: AmendmentId,
+    val date: LocalDateTime,
+    val status: AmendmentStatus,
+    val type: AmendmentType,
+    val relatesTo: AmendmentRelatesTo,
+    val relatedItem: String,
+    val xTOKEN: Token
+) {
+    data class Document(
+        val documentType: DocumentType,
+        val id: DocumentId,
+        val title: String,
+        val description: String?
+    )
+}

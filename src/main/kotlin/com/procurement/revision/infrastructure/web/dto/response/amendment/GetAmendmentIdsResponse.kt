55c4da9c0a717311539723c6b3042f31a14482cd
katherine <riodael@gmail.com>
2020-02-19 18:55:22 +0200

    ES-2521 Removed unused GetAmendmentIdsResponse class

--- src/main/kotlin/com/procurement/revision/infrastructure/web/dto/response/amendment/GetAmendmentIdsResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.procurement.revision.infrastructure.web.dto.response.amendment
-
-import com.procurement.revision.domain.model.amendment.AmendmentId
-
-class GetAmendmentIdsResponse(val id: AmendmentId)

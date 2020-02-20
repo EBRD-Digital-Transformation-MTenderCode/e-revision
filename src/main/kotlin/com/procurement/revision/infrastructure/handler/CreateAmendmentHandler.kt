b534885bbf6f80aa50f34a3f4030111020a2bcb5
katherine <riodael@gmail.com>
2020-02-20 18:27:21 +0200

    ES-2521 Rewrote CreateAmendmentHandler to inherit from AbstractHistoricalHandler

--- src/main/kotlin/com/procurement/revision/infrastructure/handler/CreateAmendmentHandler.kt
+++ src/main/kotlin/com/procurement/revision/infrastructure/handler/CreateAmendmentHandler.kt
@@ -5,48 +5,22 @@ import com.procurement.revision.application.model.amendment.CreateAmendmentResul
 import com.procurement.revision.application.service.AmendmentService
 import com.procurement.revision.infrastructure.converter.convert
 import com.procurement.revision.infrastructure.repository.HistoryRepository
-import com.procurement.revision.infrastructure.utils.toJson
 import com.procurement.revision.infrastructure.utils.toObject
-import com.procurement.revision.infrastructure.web.dto.ApiResponse2
-import com.procurement.revision.infrastructure.web.dto.ApiSuccessResponse2
-import com.procurement.revision.infrastructure.web.dto.getAction
+import com.procurement.revision.infrastructure.web.dto.Command2Type
 import com.procurement.revision.infrastructure.web.dto.getBy
-import com.procurement.revision.infrastructure.web.dto.getId
-import com.procurement.revision.infrastructure.web.dto.getVersion
 import com.procurement.revision.infrastructure.web.dto.request.amendment.CreateAmendmentRequest
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class CreateAmendmentHandler(
     private val amendmentService: AmendmentService,
-    private val historyRepository: HistoryRepository
-) {
+    historyRepository: HistoryRepository
+) : AbstractHistoricalHandler<Command2Type, CreateAmendmentResult>(CreateAmendmentResult::class.java, historyRepository) {
+    override val action: Command2Type = Command2Type.CREATE_AMENDMENT
 
-    companion object {
-        private val log = LoggerFactory.getLogger(CreateAmendmentHandler::class.java)
-    }
-
-    fun handle(node: JsonNode): ApiResponse2 {
-        val id = node.getId()
-        val action = node.getAction().toString()
-        val version = node.getVersion()
-
-        val history = historyRepository.getHistory(id.toString(), action)
-        if (history != null) {
-            val result = history.jsonData.toObject(CreateAmendmentResult::class.java)
-            return ApiSuccessResponse2(version = version, id = id, result = result)
-        }
-
+    override fun execute(node: JsonNode): CreateAmendmentResult {
         val request = node.getBy("params").toObject(CreateAmendmentRequest::class.java)
         val params = request.convert()
-
-        val result = amendmentService.createAmendment(params)
-
-        historyRepository.saveHistory(id.toString(), action, result)
-        if (log.isDebugEnabled)
-            log.debug("Amendment has been created. Response: ${result.toJson()}")
-
-        return ApiSuccessResponse2(version = version, id = id, result = result)
+        return amendmentService.createAmendment(params)
     }
 }

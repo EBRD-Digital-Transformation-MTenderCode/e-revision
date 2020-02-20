8a5cdd0baadf986b66d757ee68429c06b2290c2d
katherine <riodael@gmail.com>
2020-02-20 19:00:40 +0200

    ES-2521 Rewrote GetAmendmentIdsHandler to inherit from AbstractHandler

--- src/main/kotlin/com/procurement/revision/infrastructure/handler/GetAmendmentIdsHandler.kt
+++ src/main/kotlin/com/procurement/revision/infrastructure/handler/GetAmendmentIdsHandler.kt
@@ -2,37 +2,22 @@ package com.procurement.revision.infrastructure.handler
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.procurement.revision.application.service.AmendmentService
+import com.procurement.revision.domain.model.amendment.AmendmentId
 import com.procurement.revision.infrastructure.converter.convert
-import com.procurement.revision.infrastructure.utils.toJson
 import com.procurement.revision.infrastructure.utils.toObject
-import com.procurement.revision.infrastructure.web.dto.ApiResponse2
-import com.procurement.revision.infrastructure.web.dto.ApiSuccessResponse2
+import com.procurement.revision.infrastructure.web.dto.Command2Type
 import com.procurement.revision.infrastructure.web.dto.getBy
-import com.procurement.revision.infrastructure.web.dto.getId
-import com.procurement.revision.infrastructure.web.dto.getVersion
 import com.procurement.revision.infrastructure.web.dto.request.amendment.GetAmendmentIdsRequest
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
-class GetAmendmentIdsHandler(private val amendmentService: AmendmentService) {
+class GetAmendmentIdsHandler(private val amendmentService: AmendmentService) : AbstractHandler<Command2Type, List<AmendmentId>>() {
 
-    companion object {
-        private val log = LoggerFactory.getLogger(GetAmendmentIdsHandler::class.java)
-    }
+    override val action: Command2Type = Command2Type.GET_AMENDMENTS_IDS
 
-    fun handle(node: JsonNode): ApiResponse2 {
+    override fun execute(node: JsonNode): List<AmendmentId> {
         val request = node.getBy("params").toObject(GetAmendmentIdsRequest::class.java)
         val params = request.convert()
-        val result = amendmentService.getAmendmentIdsBy(params)
-
-        if (log.isDebugEnabled)
-            log.debug("Amendment ids have been found. Result: ${result.toJson()}")
-
-        return ApiSuccessResponse2(
-            version = node.getVersion(),
-            id = node.getId(),
-            result = result
-        )
+        return amendmentService.getAmendmentIdsBy(params)
     }
 }

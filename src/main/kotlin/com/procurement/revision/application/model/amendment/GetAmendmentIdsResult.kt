6bf9fce7d999b6a695ccaef8a930cd8295cbb18b
katherine <riodael@gmail.com>
2020-02-20 14:58:15 +0200

    ES-2521 Removed GetAmendmentIdsResult

--- src/main/kotlin/com/procurement/revision/application/model/amendment/GetAmendmentIdsResult.kt
+++
@@ -1,7 +0,0 @@
-package com.procurement.revision.application.model.amendment
-
-import com.procurement.revision.domain.model.amendment.AmendmentId
-
-data class GetAmendmentIdsResult(
-    val id: AmendmentId
-)

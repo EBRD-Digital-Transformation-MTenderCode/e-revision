4c673e868fb0e1fe70c8d0bebe883e8648a1493b
katherine <riodael@gmail.com>
2020-02-21 16:43:58 +0200

    ES-2521 Added data keyword to CreateAmendmentParams class

--- src/main/kotlin/com/procurement/revision/application/model/amendment/CreateAmendmentParams.kt
+++ src/main/kotlin/com/procurement/revision/application/model/amendment/CreateAmendmentParams.kt
@@ -8,20 +8,20 @@ import com.procurement.revision.infrastructure.model.OperationType
 import java.time.LocalDateTime
 import java.util.*
 
-class CreateAmendmentParams(
-     val amendment: Amendment,
-     val id: UUID,
-     val operationType: OperationType,
-     val startDate: LocalDateTime,
-     val cpid: String,
-     val ocid: String,
-     val owner: Owner
+data class CreateAmendmentParams(
+    val amendment: Amendment,
+    val id: UUID,
+    val operationType: OperationType,
+    val startDate: LocalDateTime,
+    val cpid: String,
+    val ocid: String,
+    val owner: Owner
 ) {
     data class Amendment(
-         val rationale: String,
-         val description: String?,
-         val documents: List<Document>,
-         val id: AmendmentId
+        val rationale: String,
+        val description: String?,
+        val documents: List<Document>,
+        val id: AmendmentId
     ) {
         data class Document(
             val documentType: DocumentType,

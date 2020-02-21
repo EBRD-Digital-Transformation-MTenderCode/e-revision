ef313cca053147099c4bfc1f766f6a64cda62275
katherine <riodael@gmail.com>
2020-02-21 12:41:36 +0200

    ES-2521 Added ValidationError and ValidationResult

---
+++ src/main/kotlin/com/procurement/revision/infrastructure/handler/validation/ValidationError.kt
@@ -0,0 +1,12 @@
+package com.procurement.revision.infrastructure.handler.validation
+
+import com.procurement.revision.domain.model.document.DocumentId
+
+sealed class ValidationError(val code: String, val description: String) {
+
+    class InvalidDocumentType(documentId: DocumentId) : ValidationError(
+        code = "10.77",
+        description = "Document '${documentId}' has invalid documentType."
+    )
+}
+

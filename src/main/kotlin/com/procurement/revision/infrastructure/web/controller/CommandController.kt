63554d18db06130760e889011f3ae2a61c324a3a
katherine <riodael@gmail.com>
2020-02-26 17:54:15 +0200

    ES-2521 Fixed response generation in controller

--- src/main/kotlin/com/procurement/revision/infrastructure/web/controller/CommandController.kt
+++ src/main/kotlin/com/procurement/revision/infrastructure/web/controller/CommandController.kt
@@ -1,11 +1,16 @@
 package com.procurement.revision.infrastructure.web.controller
 
 import com.procurement.revision.domain.util.Result
+import com.procurement.revision.infrastructure.configuration.properties.GlobalProperties
+import com.procurement.revision.infrastructure.exception.Fail
 import com.procurement.revision.infrastructure.service.CommandService
 import com.procurement.revision.infrastructure.utils.toJson
 import com.procurement.revision.infrastructure.utils.tryToNode
 import com.procurement.revision.infrastructure.web.dto.ApiResponse
+import com.procurement.revision.infrastructure.web.dto.ApiVersion
+import com.procurement.revision.infrastructure.web.dto.NaN
 import com.procurement.revision.infrastructure.web.dto.errorResponse
+import com.procurement.revision.infrastructure.web.dto.generateResponseOnFailure
 import com.procurement.revision.infrastructure.web.dto.tryGetAction
 import com.procurement.revision.infrastructure.web.dto.tryGetId
 import com.procurement.revision.infrastructure.web.dto.tryGetVersion
@@ -17,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/command")
@@ -33,24 +39,24 @@ class CommandController(private val commandService: CommandService) {
         val node = when (val result = requestBody.tryToNode()) {
             is Result.Success -> result.get
             is Result.Failure -> {
-                log.debug("Error.", result.error.message)
-                val response = errorResponse(exception = result.error)
+                log.debug("Error.", result.error.description)
+                val response = generateResponse(fail = result.error)
                 return ResponseEntity(response, HttpStatus.OK)
             }
         }
         val id = when (val result = node.tryGetId()) {
             is Result.Success -> result.get
             is Result.Failure -> {
-                log.debug("Error.", result.error.message)
-                val response = errorResponse(exception = result.error)
+                log.debug("Error.", result.error.description)
+                val response = generateResponse(fail = result.error)
                 return ResponseEntity(response, HttpStatus.OK)
             }
         }
         val version = when (val result = node.tryGetVersion()) {
             is Result.Success -> result.get
             is Result.Failure -> {
-                log.debug("Error.", result.error.message)
-                val response = errorResponse(exception = result.error, id = id)
+                log.debug("Error.", result.error.description)
+                val response = generateResponse(fail = result.error, id = id)
                 return ResponseEntity(response, HttpStatus.OK)
             }
         }
@@ -58,8 +64,8 @@ class CommandController(private val commandService: CommandService) {
         when (val result = node.tryGetAction()) {
             is Result.Success -> result.get
             is Result.Failure -> {
-                log.debug("Error.", result.error.message)
-                val response = errorResponse(exception = result.error, id = id, version = version)
+                log.debug("Error.", result.error.description)
+                val response = generateResponse(fail = result.error, id = id, version = version)
                 return ResponseEntity(response, HttpStatus.OK)
             }
         }
@@ -80,4 +86,10 @@ class CommandController(private val commandService: CommandService) {
         }
         return ResponseEntity(response, HttpStatus.OK)
     }
+
+    private fun generateResponse(
+        fail: Fail,
+        version: ApiVersion = GlobalProperties.App.apiVersion,
+        id: UUID = NaN
+    ) = generateResponseOnFailure(fail = fail, id = id, version = version)
 }

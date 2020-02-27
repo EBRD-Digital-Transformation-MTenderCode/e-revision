758e0407d7ce0983bf1f7b656ecc0be4a0e8f2c7
katherine <riodael@gmail.com>
2020-02-27 18:41:33 +0200

    ES-2521 DataValidationConverter refactoring in progress

--- src/main/kotlin/com/procurement/revision/application/model/amendment/DataValidationParams.kt
+++ src/main/kotlin/com/procurement/revision/application/model/amendment/DataValidationParams.kt
@@ -1,25 +1,135 @@
 package com.procurement.revision.application.model.amendment
 
 import com.procurement.revision.domain.enums.DocumentType
+import com.procurement.revision.domain.util.Option
+import com.procurement.revision.domain.util.Result
+import com.procurement.revision.domain.util.flatMap
+import com.procurement.revision.infrastructure.fail.Fail
+import com.procurement.revision.infrastructure.fail.error.ValidationError
 import com.procurement.revision.infrastructure.model.OperationType
+import java.util.*
 
-data class DataValidationParams(
-
+data class DataValidationParams private constructor(
     val amendments: List<Amendment>,
     val cpid: String,
     val ocid: String,
     val operationType: OperationType
 ) {
-    data class Amendment(
+    companion object {
+        fun create(
+            amendments: List<Amendment>,
+            cpid: String,
+            ocid: String,
+            operationType: String
+        ): Result<DataValidationParams, Fail> {
+            if (amendments.isEmpty()) {
+                return Result.failure(ValidationError.EmptyCollection("documents", "amendment"))
+            }
+
+            return OperationType.tryFromString(operationType)
+                .flatMap { type ->
+                    Result.success(
+                        DataValidationParams(
+                            cpid = cpid,
+                            ocid = ocid,
+                            operationType = type,
+                            amendments = amendments
+                        )
+                    )
+                }
+        }
+    }
+
+    data class Amendment private constructor(
+        val id: UUID,
         val rationale: String,
         val description: String?,
         val documents: List<Document>
     ) {
-        data class Document(
+        companion object {
+            fun create(
+                id: UUID,
+                rationale: String,
+                description: String?,
+                documents: Option<List<Document>>
+            ): Result<Amendment, Fail> {
+                if (documents.isDefined && documents.get.isEmpty())
+                    return Result.failure(ValidationError.EmptyCollection("documents", "amendment"))
+
+                return Result.success(
+                    Amendment(
+                        id = id,
+                        rationale = rationale,
+                        description = description,
+                        documents = if (documents.isDefined) documents.get else emptyList()
+                    )
+                )
+            }
+
+            /*fun create(
+                rationale: String,
+                description: String?,
+                documents: Result<List<Document>?, Fail>
+            ): Result<Amendment, Fail> {
+                val docs = when(documents) {
+                    is Result.Success -> documents.get
+                    is Result.Failure -> return Result.failure(documents.error)
+                }
+                if(docs != null && docs.isEmpty())
+                    return Result.failure(ValidationError.EmptyCollection("documents", "amendment"))
+
+                return Result.success(
+                    Amendment(
+                        rationale = rationale,
+                        description = description,
+                        documents = docs ?: emptyList()
+                    )
+                )
+            }*/
+        }
+
+        override fun equals(other: Any?): Boolean = if (this === other)
+            true
+        else
+            other is Amendment
+                && this.id == other.id
+
+        override fun hashCode(): Int = id.hashCode()
+
+        class Document private constructor(
             val documentType: DocumentType,
             val id: String,
             val title: String,
             val description: String?
-        )
+        ) {
+            companion object {
+                fun tryCreate(
+                    documentType: String,
+                    id: String,
+                    title: String,
+                    description: String?
+                ): Result<Document, Fail> = DocumentType.tryFromString(documentType)
+                    .flatMap { type ->
+                        Result.success(
+                            Document(
+                                id = id,
+                                description = description,
+                                documentType = type,
+                                title = title
+                            )
+                        )
+                    }
+            }
+
+            override fun equals(other: Any?): Boolean = if (this === other)
+                true
+            else
+                other is Document
+                    && this.id == other.id
+
+            override fun hashCode(): Int = id.hashCode()
+        }
     }
 }
+
+
--- src/main/kotlin/com/procurement/revision/infrastructure/converter/DataValidationConverter.kt
+++ src/main/kotlin/com/procurement/revision/infrastructure/converter/DataValidationConverter.kt
@@ -1,35 +1,41 @@
 package com.procurement.revision.infrastructure.converter
 
-import com.procurement.revision.application.exception.ErrorException
-import com.procurement.revision.application.exception.ErrorType
 import com.procurement.revision.application.model.amendment.DataValidationParams
-import com.procurement.revision.domain.enums.DocumentType
-import com.procurement.revision.infrastructure.model.OperationType
+import com.procurement.revision.domain.util.Result
+import com.procurement.revision.domain.util.extension.mapOptionalResult
+import com.procurement.revision.domain.util.extension.mapResult
+import com.procurement.revision.infrastructure.fail.Fail
 import com.procurement.revision.infrastructure.web.dto.request.amendment.DataValidationRequest
-import com.procurement.revision.lib.errorIfEmpty
 
-fun DataValidationRequest.convert() = DataValidationParams(
-    amendments = amendments.map { amendment ->
-        DataValidationParams.Amendment(
-            rationale = amendment.rationale,
-            description = amendment.description,
-            documents = amendment.documents.errorIfEmpty {
-                ErrorException(
-                    error = ErrorType.IS_EMPTY,
-                    message = "The amendment with cpid '${cpid}' contains empty list of documents."
-                )
-            }?.map { document ->
-                DataValidationParams.Amendment.Document(
-                    id = document.id,
-                    description = document.description,
-                    documentType = DocumentType.fromString(document.documentType),
-                    title = document.title
-                )
-            }.orEmpty()
-        )
-    },
-    ocid = ocid,
-    cpid = cpid,
-    operationType = OperationType.fromString(operationType)
-)
+fun DataValidationRequest.convert(): Result<DataValidationParams, Fail> {
+    val amendments = amendments.mapResult { it.convert() }
+    if (amendments.isFail)
+        return Result.failure(amendments.error)
 
+    return DataValidationParams.create(
+        amendments = amendments.get,
+        ocid = ocid,
+        cpid = cpid,
+        operationType = operationType
+    )
+}
+
+private fun DataValidationRequest.Amendment.convert(): Result<DataValidationParams.Amendment, Fail> {
+    val documents = this.documents.mapOptionalResult { it.convert() }
+    if (documents.isFail)
+        return Result.failure(documents.error)
+
+    return DataValidationParams.Amendment.create(
+        rationale = this.rationale,
+        description = this.description,
+        documents = documents.get
+    )
+}
+
+private fun DataValidationRequest.Amendment.Document.convert(): Result<DataValidationParams.Amendment.Document, Fail> =
+    DataValidationParams.Amendment.Document.tryCreate(
+        documentType = this.documentType,
+        id = this.id,
+        title = this.title,
+        description = this.description
+    )

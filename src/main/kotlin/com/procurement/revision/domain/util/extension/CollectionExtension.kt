6167b183dcd58dffedfe2e2c0151d471b6f113c7
katherine <riodael@gmail.com>
2020-02-27 18:39:49 +0200

    ES-2521 Added collection extensions with methods for converting collection to Result

---
+++ src/main/kotlin/com/procurement/revision/domain/util/extension/CollectionExtension.kt
@@ -0,0 +1,29 @@
+package com.procurement.revision.domain.util.extension
+
+import com.procurement.revision.domain.util.Option
+import com.procurement.revision.domain.util.Result
+
+fun <T, R, E> List<T>?.mapOptionalResult(block: (T) -> Result<R, E>): Result<Option<List<R>>, E> {
+    if (this == null)
+        return Result.success(Option.none())
+
+    val r = mutableListOf<R>()
+    for (element in this) {
+        when (val result = block(element)) {
+            is Result.Success -> r.add(result.get)
+            is Result.Failure -> return result
+        }
+    }
+    return Result.success(Option.pure(r))
+}
+
+fun <T, R, E> List<T>.mapResult(block: (T) -> Result<R, E>): Result<List<R>, E> {
+    val r = mutableListOf<R>()
+    for (element in this) {
+        when (val result = block(element)) {
+            is Result.Success -> r.add(result.get)
+            is Result.Failure -> return result
+        }
+    }
+    return Result.success(r)
+}

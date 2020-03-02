ae4974afe009f8e74da54ee0f49c76b4fc0affcb
katherine <riodael@gmail.com>
2020-03-02 16:57:35 +0200

    ES-2521 Refactored utils and moved to package functional

--- src/main/kotlin/com/procurement/revision/domain/functional/Option.kt
+++ src/main/kotlin/com/procurement/revision/domain/functional/Option.kt
@@ -1,18 +1,20 @@
-package com.procurement.revision.domain.util
+package com.procurement.revision.domain.functional
 
 sealed class Option<out T> {
+
     companion object {
         fun <T> pure(value: T): Option<T> = Some(
             value
         )
         fun <T> none(): Option<T> = None
-        fun <T> fromNullable(a: T?): Option<T> = if (a != null) Some(
-            a
+        fun <T> fromNullable(value: T?): Option<T> = if (value != null) Some(
+            value
         ) else None
     }
 
     abstract val get: T
     abstract val orNull: T?
+
     fun <E : Exception> orThrow(block: () -> E): T = when (this) {
         is Some -> get
         is None -> throw block()
@@ -21,21 +23,22 @@ sealed class Option<out T> {
     abstract val isEmpty: Boolean
     abstract val nonEmpty: Boolean
     val isDefined: Boolean
-        get() = isEmpty
+        get() = !isEmpty
 
-    fun <R> map(block: (T) -> R): Option<R> = flatMap {
-        Some(
-            block(it)
+    fun <R> map(transform: (T) -> R): Option<R> = when (this) {
+        is Some -> Some(
+            transform(this.get)
         )
+        is None -> this
     }
 
-    fun <R> flatMap(block: (T) -> Option<R>): Option<R> = when (this) {
+    fun <R> bind(function: (T) -> Option<R>): Option<R> = when (this) {
+        is Some -> function(this.get)
         is None -> this
-        is Some -> block(this.value)
     }
 }
 
-data class Some<out T>(val value: T) : Option<T>() {
+class Some<out T>(value: T) : Option<T>() {
     override val get: T = value
     override val orNull: T = value
     override val isEmpty: Boolean = false
@@ -43,7 +46,8 @@ data class Some<out T>(val value: T) : Option<T>() {
 }
 
 object None : Option<Nothing>() {
-    override val get: Nothing = throw NoSuchElementException("Option do not contains value.")
+    override val get: Nothing
+        get() = throw NoSuchElementException("Option do not contain value.")
     override val orNull: Nothing? = null
     override val isEmpty: Boolean = true
     override val nonEmpty: Boolean = !isEmpty

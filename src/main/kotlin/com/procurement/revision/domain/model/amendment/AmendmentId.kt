9427de0b41f5d74fdf0ef678437ab8834daf6345
katherine <riodael@gmail.com>
2020-03-02 17:51:48 +0200

    ES-2521 Added try methods for ids typealias

--- src/main/kotlin/com/procurement/revision/domain/model/amendment/AmendmentId.kt
+++ src/main/kotlin/com/procurement/revision/domain/model/amendment/AmendmentId.kt
@@ -1,5 +1,11 @@
 package com.procurement.revision.domain.model.amendment
 
+import com.procurement.revision.domain.functional.Result
+import com.procurement.revision.domain.util.extension.tryUUID
+import com.procurement.revision.infrastructure.fail.error.RequestError
 import java.util.*
 
 typealias AmendmentId = UUID
+
+fun String.tryAmendmentId(): Result<AmendmentId, RequestError.ParsingError> =
+    this.tryUUID(ofEntity = "amendment")
--- src/main/kotlin/com/procurement/revision/domain/model/document/DocumentId.kt
+++ src/main/kotlin/com/procurement/revision/domain/model/document/DocumentId.kt
@@ -1,3 +1,9 @@
 package com.procurement.revision.domain.model.document
 
+import com.procurement.revision.domain.functional.Result
+import com.procurement.revision.infrastructure.fail.error.RequestError
+
 typealias DocumentId = String
+
+fun String.tryDocumentId(): Result<DocumentId, RequestError.ParsingError> =
+    Result.success(this)

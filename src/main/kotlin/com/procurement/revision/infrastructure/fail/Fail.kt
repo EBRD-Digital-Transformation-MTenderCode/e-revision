ea21a33eb2acfa50a78d12f1773d9700a7b596a9
katherine <riodael@gmail.com>
2020-03-03 14:33:35 +0200

    ES-2521 Refactored Fail and added DataErrors

--- src/main/kotlin/com/procurement/revision/infrastructure/fail/Fail.kt
+++ src/main/kotlin/com/procurement/revision/infrastructure/fail/Fail.kt
@@ -1,11 +1,31 @@
 package com.procurement.revision.infrastructure.fail
 
-sealed class Fail(val code: String, val description: String) {
+import com.procurement.revision.domain.functional.Result
+import com.procurement.revision.domain.functional.ValidationResult
 
-    abstract class Error(code: String, description: String) : Fail(code, description)
-    abstract class Incident(code: String, description: String) : Fail(code, description)
+sealed class Fail {
+    abstract class Error(val prefix: String) : Fail() {
+        abstract val code: String
+        abstract val description: String
+        val message: String
+            get() = "ERROR CODE: '$code', DESCRIPTION: '$description'."
+
+        companion object {
+            fun <T, E : Error> E.toResult(): Result<T, E> = Result.failure(this)
+            fun <E : Error> E.toValidationResult(): ValidationResult<E> = ValidationResult.error(this)
+        }
+    }
+
+    sealed class Incident : Fail() {
+        abstract val code: String
+        abstract val description: String
+
+        class DatabaseInteractionIncident(message: String?) : Incident() {
+            override val code = "20.00"
+            override val description = "Database incident. $message"
+        }
+    }
 }
-
 
 
 

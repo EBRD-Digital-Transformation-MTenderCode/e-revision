aec026affb9c22a28c0ae94f1f7fba8501a1ccd6
katherine <riodael@gmail.com>
2020-03-04 17:43:52 +0200

    ES-2521 Removed enums de/serializers

--- src/main/kotlin/com/procurement/revision/infrastructure/bind/jackson/configuration.kt
+++ src/main/kotlin/com/procurement/revision/infrastructure/bind/jackson/configuration.kt
@@ -5,19 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.databind.node.JsonNodeFactory
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import com.procurement.revision.domain.enums.AmendmentRelatesTo
-import com.procurement.revision.domain.enums.AmendmentStatus
-import com.procurement.revision.domain.enums.AmendmentType
-import com.procurement.revision.domain.enums.DocumentType
-import com.procurement.revision.domain.enums.MainProcurementCategory
-import com.procurement.revision.domain.enums.ProcurementMethod
 import com.procurement.revision.infrastructure.bind.apiversion.ApiVersionDeserializer
 import com.procurement.revision.infrastructure.bind.apiversion.ApiVersionSerializer
-import com.procurement.revision.infrastructure.bind.databinding.EnumDeserializer
-import com.procurement.revision.infrastructure.bind.databinding.EnumSerializer
 import com.procurement.revision.infrastructure.bind.databinding.JsonDateTimeDeserializer
 import com.procurement.revision.infrastructure.bind.databinding.JsonDateTimeSerializer
-import com.procurement.revision.infrastructure.model.OperationType
 import com.procurement.revision.infrastructure.web.dto.ApiVersion
 import java.time.LocalDateTime
 
@@ -34,31 +25,6 @@ fun ObjectMapper.configuration() {
          */
         addSerializer(ApiVersion::class.java, ApiVersionSerializer())
         addDeserializer(ApiVersion::class.java, ApiVersionDeserializer())
-
-        /**
-         *  Serializer/Deserializer for enum type
-         */
-        addDeserializer(AmendmentStatus::class.java, EnumDeserializer(AmendmentStatus))
-        addSerializer(AmendmentStatus::class.java, EnumSerializer<AmendmentStatus>())
-
-        addDeserializer(AmendmentType::class.java, EnumDeserializer(AmendmentType))
-        addSerializer(AmendmentType::class.java, EnumSerializer<AmendmentType>())
-
-        addDeserializer(DocumentType::class.java, EnumDeserializer(DocumentType))
-        addSerializer(DocumentType::class.java, EnumSerializer<DocumentType>())
-
-        addDeserializer(AmendmentRelatesTo::class.java, EnumDeserializer(AmendmentRelatesTo))
-        addSerializer(AmendmentRelatesTo::class.java, EnumSerializer<AmendmentRelatesTo>())
-
-        addDeserializer(OperationType::class.java, EnumDeserializer(OperationType))
-        addSerializer(OperationType::class.java, EnumSerializer<OperationType>())
-
-        addDeserializer(ProcurementMethod::class.java, EnumDeserializer(ProcurementMethod))
-        addSerializer(ProcurementMethod::class.java, EnumSerializer<ProcurementMethod>())
-
-        addDeserializer(MainProcurementCategory::class.java, EnumDeserializer(MainProcurementCategory))
-        addSerializer(MainProcurementCategory::class.java, EnumSerializer<MainProcurementCategory>())
-
     }
 
     this.registerModule(module)

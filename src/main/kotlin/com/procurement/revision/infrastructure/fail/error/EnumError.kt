aba8bf3d103edf366da3a06a78f554cbad98cdbf
katherine <riodael@gmail.com>
2020-03-04 17:46:51 +0200

    ES-2521 Removed redundant EnumError

--- src/main/kotlin/com/procurement/revision/infrastructure/fail/error/EnumError.kt
+++
@@ -1,8 +0,0 @@
-package com.procurement.revision.infrastructure.fail.error
-
-import com.procurement.revision.infrastructure.fail.Fail
-
-class EnumError(enumType: String, value: String, values: String) : Fail.Error("VR-") {
-    override val code: String = "${prefix}1"
-    override val description = "Unknown value for enumType $enumType: $value, Allowed values are $values"
-}

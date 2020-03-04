2d553e7aeb375d589f343211364afa8662bb13f9
katherine <riodael@gmail.com>
2020-03-04 17:39:54 +0200

    ES-2521 Renamed EnumException to EnumElementProviderException

--- src/main/kotlin/com/procurement/revision/domain/exception/EnumElementProviderException.kt
+++ src/main/kotlin/com/procurement/revision/domain/exception/EnumElementProviderException.kt
@@ -1,6 +1,6 @@
 package com.procurement.revision.domain.exception
 
-data class EnumException(private val enumType: String, val value: String, val values: String) : RuntimeException(
+data class EnumElementProviderException(private val enumType: String, val value: String, val values: String) : RuntimeException(
     "Unknown value for enumType $enumType: $value, Allowed values are $values"
 ) {
     val code: String = "00.00"

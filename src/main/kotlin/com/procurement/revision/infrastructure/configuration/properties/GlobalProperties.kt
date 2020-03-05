c92fd677cff1f9da62fd45f9b23b58f0ddec3a5b
katherine <riodael@gmail.com>
2020-03-05 11:05:05 +0200

    ES-2521 Added getGitProperties method to GlobalProperties

--- src/main/kotlin/com/procurement/revision/infrastructure/configuration/properties/GlobalProperties.kt
+++ src/main/kotlin/com/procurement/revision/infrastructure/configuration/properties/GlobalProperties.kt
@@ -1,12 +1,29 @@
 package com.procurement.revision.infrastructure.configuration.properties
 
 import com.procurement.revision.infrastructure.web.dto.ApiVersion
+import java.util.*
 
 object GlobalProperties {
-    const val serviceId = "21"
-    const val serviceName = "e-revision"
+    val service = Service()
 
     object App {
         val apiVersion = ApiVersion(major = 1, minor = 0, patch = 0)
+    }
+
+    class Service(
+        val id: String = "21",
+        val name: String = "e-revision",
+        val version: String = getGitProperties()
+    )
+    private fun getGitProperties():String {
+        val prop = Properties()
+        val loader = Thread.currentThread().contextClassLoader
+        val stream = loader.getResourceAsStream("git.properties")
+        if (stream != null) {
+            prop.load(stream)
+            return prop.getProperty("git.commit.id.abbrev")
+        } else {
+            throw RuntimeException("Unable to find git.commit.id.abbrev")
+        }
     }
 }

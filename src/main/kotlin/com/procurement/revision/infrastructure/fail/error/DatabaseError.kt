3a1cbc74b3ed9828613fcb1c6da5855406cafe91
katherine <riodael@gmail.com>
2020-03-06 11:11:21 +0200

    ES-2521 Removed unused errors

--- src/main/kotlin/com/procurement/revision/infrastructure/fail/error/DatabaseError.kt
+++
@@ -1,9 +0,0 @@
-package com.procurement.revision.infrastructure.fail.error
-
-import com.procurement.revision.infrastructure.fail.Fail
-
-sealed class DatabaseError(numberError: String, override val description: String) : Fail.Error("DB-") {
-    override val code: String = prefix + numberError
-
-    class EntityNotFoundError(id: String) : DatabaseError(numberError = "1", description = "Entity '$id' is not found.")
-}
--- src/main/kotlin/com/procurement/revision/infrastructure/fail/error/ParsingError.kt
+++
@@ -1,8 +0,0 @@
-package com.procurement.revision.infrastructure.fail.error
-
-import com.procurement.revision.infrastructure.fail.Fail
-
-class ParsingError(message: String) : Fail.Error("PR-") {
-    override val code: String = "${prefix}1"
-    override val description = "Invalid JSON. '${message}'"
-}

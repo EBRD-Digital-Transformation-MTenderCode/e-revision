15ee6f3c4d8d52713a252ca02d61acfeaceeb0d4
katherine <riodael@gmail.com>
2020-03-06 16:44:48 +0200

    ES-2521 Fixed DateTime de/serializers

--- src/main/kotlin/com/procurement/revision/infrastructure/bind/databinding/JsonDateTimeDeserializer.kt
+++ src/main/kotlin/com/procurement/revision/infrastructure/bind/databinding/JsonDateTimeDeserializer.kt
@@ -3,11 +3,12 @@ package com.procurement.revision.infrastructure.bind.databinding
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
+import com.procurement.revision.domain.model.date.parse
 import java.time.LocalDateTime
 
 class JsonDateTimeDeserializer : JsonDeserializer<LocalDateTime>() {
     companion object {
-        fun deserialize(value: String): LocalDateTime = LocalDateTime.parse(value, JsonDateTimeFormatter.formatter)
+        fun deserialize(value: String): LocalDateTime = value.parse()
     }
 
     override fun deserialize(jsonParser: JsonParser, deserializationContext: DeserializationContext): LocalDateTime =
--- src/main/kotlin/com/procurement/revision/infrastructure/bind/databinding/JsonDateTimeSerializer.kt
+++ src/main/kotlin/com/procurement/revision/infrastructure/bind/databinding/JsonDateTimeSerializer.kt
@@ -3,11 +3,12 @@ package com.procurement.revision.infrastructure.bind.databinding
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.SerializerProvider
+import com.procurement.revision.domain.model.date.format
 import java.time.LocalDateTime
 
 class JsonDateTimeSerializer : JsonSerializer<LocalDateTime>() {
     companion object {
-        fun serialize(date: LocalDateTime): String = date.format(JsonDateTimeFormatter.formatter)
+        fun serialize(date: LocalDateTime): String = date.format()
     }
 
     override fun serialize(date: LocalDateTime, jsonGenerator: JsonGenerator, provider: SerializerProvider) =

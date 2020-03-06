f7ff796a41cdd54e1c36c54b01af9db6472ebda5
katherine <riodael@gmail.com>
2020-03-06 16:45:09 +0200

    ES-2521 Removed unused tryCreateLocalDateTime method

--- src/main/kotlin/com/procurement/revision/domain/util/extension/StringExtension.kt
+++ src/main/kotlin/com/procurement/revision/domain/util/extension/StringExtension.kt
@@ -1,16 +1,7 @@
 package com.procurement.revision.domain.util.extension
 
 import com.procurement.revision.domain.functional.Result
-import com.procurement.revision.infrastructure.bind.databinding.JsonDateTimeFormatter
-import java.time.LocalDateTime
 import java.util.*
-
-fun String.tryCreateLocalDateTime() =
-    try {
-        Result.success(LocalDateTime.parse(this, JsonDateTimeFormatter.formatter))
-    } catch (ex: Exception) {
-        Result.failure("Could not convert '$this' to date")
-    }
 
 fun String.tryUUID(): Result<UUID, String> =
     try {

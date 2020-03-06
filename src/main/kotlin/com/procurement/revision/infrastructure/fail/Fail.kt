4be3a0f002aa7eac4d4b6b3409215c6c4e498b7b
katherine <riodael@gmail.com>
2020-03-06 14:54:56 +0200

    ES-2521 Added ParseFromDatabaseIncident and DatabaseIncident

--- src/main/kotlin/com/procurement/revision/infrastructure/fail/Fail.kt
+++ src/main/kotlin/com/procurement/revision/infrastructure/fail/Fail.kt
@@ -32,6 +32,18 @@ sealed class Fail {
             description = "Database consistency incident. $message"
         )
 
+        class ParseFromDatabaseIncident(jsonData: String) : Incident(
+            level = Level.ERROR,
+            number = "3",
+            description = "Could not parse data '$jsonData' stored in database. "
+        )
+
+        class DatabaseIncident : Incident(
+            level = Level.ERROR,
+            number = "4",
+            description = "Could not process data from database. "
+        )
+
         enum class Level(override val key: String) : EnumElementProvider.Key {
             ERROR("error"),
             WARNING("warning"),

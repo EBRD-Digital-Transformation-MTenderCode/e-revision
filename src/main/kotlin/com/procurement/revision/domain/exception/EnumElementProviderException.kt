e8652b9ba988c5772d020629255376da88339307
katherine <riodael@gmail.com>
2020-03-06 12:17:32 +0200

    ES-2521 Removed redundant field in EnumElementProviderException

--- src/main/kotlin/com/procurement/revision/domain/exception/EnumElementProviderException.kt
+++ src/main/kotlin/com/procurement/revision/domain/exception/EnumElementProviderException.kt
@@ -1,7 +1,6 @@
 package com.procurement.revision.domain.exception
 
-data class EnumElementProviderException(private val enumType: String, val value: String, val values: String) : RuntimeException(
-    "Unknown value for enumType $enumType: $value, Allowed values are $values"
-) {
-    val code: String = "00.00"
-}
+data class EnumElementProviderException(private val enumType: String, val value: String, val values: String) :
+    RuntimeException(
+        "Unknown value for enumType $enumType: $value, Allowed values are $values"
+    )

e7596eee04bab7a4039612f8576fca5a8747db29
katherine <riodael@gmail.com>
2020-03-06 12:18:00 +0200

    ES-2521 Removed redundant DatabaseInteractionException

--- src/main/kotlin/com/procurement/revision/infrastructure/exception/DatabaseInteractionException.kt
+++
@@ -1,6 +0,0 @@
-package com.procurement.revision.infrastructure.exception
-
-class DatabaseInteractionException : RuntimeException {
-    constructor(message: String, cause: Throwable) : super(message, cause)
-    constructor(message: String) : super(message)
-}

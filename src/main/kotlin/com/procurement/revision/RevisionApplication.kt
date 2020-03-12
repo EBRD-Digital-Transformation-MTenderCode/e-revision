9cd3bb09a6a70a5ec8caa66abf8646dab76599be
katherine <riodael@gmail.com>
2020-03-12 11:27:03 +0200

    ES-2521 Added println to main method for testing purpose

--- src/main/kotlin/com/procurement/revision/RevisionApplication.kt
+++ src/main/kotlin/com/procurement/revision/RevisionApplication.kt
@@ -1,6 +1,7 @@
 package com.procurement.revision
 
 import com.procurement.revision.infrastructure.configuration.ApplicationConfiguration
+import com.procurement.revision.infrastructure.configuration.properties.GlobalProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -9,5 +10,5 @@ class RevisionApplication
 
 fun main(args: Array<String>) {
     runApplication<RevisionApplication>(*args)
+    println("Ran service ${GlobalProperties.service.name}:${GlobalProperties.service.version}")
 }
-

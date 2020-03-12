1ccdcfd09095dab176ddd46f4bd52a856e01ffdc
katherine <riodael@gmail.com>
2020-03-12 12:35:36 +0200

    ES-2521 Removed Properties.load method and refactored loadVersion using getResourceAsStream

--- src/main/kotlin/com/procurement/revision/infrastructure/configuration/properties/GlobalProperties.kt
+++ src/main/kotlin/com/procurement/revision/infrastructure/configuration/properties/GlobalProperties.kt
@@ -1,6 +1,5 @@
 package com.procurement.revision.infrastructure.configuration.properties
 
-import com.procurement.revision.infrastructure.io.load
 import com.procurement.revision.infrastructure.io.orThrow
 import com.procurement.revision.infrastructure.web.dto.ApiVersion
 import java.util.*
@@ -18,9 +17,14 @@ object GlobalProperties {
         val version: String = loadVersion()
 
         private fun loadVersion(): String {
-            val pathToFile = "file:/maven/e-revision-1.1.1.jar/BOOT-INF/classes/git.properties"
-                //this.javaClass.getResourcePath("git.properties")
-            val gitProps: Properties = Properties().load(pathToFile = pathToFile)
+            val gitProps: Properties = try {
+                GlobalProperties::class.java.getResourceAsStream("/git.properties")
+                    .use { stream ->
+                        Properties().apply { load(stream) }
+                    }
+            } catch (expected: Exception) {
+                throw IllegalStateException(expected)
+            }
             return gitProps.orThrow("git.commit.id.abbrev")
         }
     }
--- src/main/kotlin/com/procurement/revision/infrastructure/io/properties.kt
+++ src/main/kotlin/com/procurement/revision/infrastructure/io/properties.kt
@@ -1,19 +1,6 @@
 package com.procurement.revision.infrastructure.io
 
-import java.io.FileNotFoundException
-import java.io.FileReader
 import java.util.*
-
-fun Properties.load(pathToFile: String) = try {
-    this.apply {
-        FileReader(pathToFile)
-            .use { reader ->
-                this.load(reader)
-            }
-    }
-} catch (expected: FileNotFoundException) {
-    throw IllegalStateException("File '$pathToFile' is not found.")
-}
 
 fun Properties.orThrow(name: String): String = this[name]
     ?.toString()

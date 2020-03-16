65e7395809b6df4834a3113da9b88b4c1c689d0b
katherine <riodael@gmail.com>
2020-03-16 16:22:27 +0200

    ES-2592 Externalised regex creation outside tryCreate method

--- src/main/kotlin/com/procurement/revision/domain/model/Cpid.kt
+++ src/main/kotlin/com/procurement/revision/domain/model/Cpid.kt
@@ -7,14 +7,13 @@ import com.procurement.revision.domain.functional.Result.Companion.success
 class Cpid private constructor(val value: String) {
 
     companion object {
-        private const val pattern = "^([a-z]{4})-([a-z0-9]{6})-([A-Z]{2})-[0-9]{13}\$"
+        private val regex = "^([a-z]{4})-([a-z0-9]{6})-([A-Z]{2})-[0-9]{13}\$".toRegex()
 
         fun tryCreate(cpid: String): Result<Cpid, String> {
-            val regex = Regex(pattern)
             return if (cpid.matches(regex = regex))
                 success(Cpid(value = cpid))
             else
-                failure(pattern)
+                failure(regex.pattern)
         }
     }
 }
--- src/main/kotlin/com/procurement/revision/domain/model/Ocid.kt
+++ src/main/kotlin/com/procurement/revision/domain/model/Ocid.kt
@@ -4,14 +4,13 @@ import com.procurement.revision.domain.functional.Result
 
 class Ocid private constructor(val value: String) {
     companion object {
-        private const val pattern = "^([a-z]{4})-([a-z0-9]{6})-([A-Z]{2})-([0-9]{13})-([A-Z]{2})-([0-9]{13})\$"
+        private val regex = "^([a-z]{4})-([a-z0-9]{6})-([A-Z]{2})-([0-9]{13})-([A-Z]{2})-([0-9]{13})\$".toRegex()
 
         fun tryCreate(ocid: String): Result<Ocid, String> {
-            val regex = Regex(pattern)
             return if (ocid.matches(regex = regex))
                 Result.success(Ocid(value = ocid))
             else
-                Result.failure(pattern)
+                Result.failure(regex.pattern)
         }
     }
 }

b94d47e56633ee05cd92f3cfd8f3149e9c87920b
katherine <kate.shaidurova@gmail.com>
2020-03-23 17:36:30 +0200

    ES-2675 Replaced value call with toString call for Ocid and Cpid objects

--- src/test/kotlin/com/procurement/revision/application/service/AmendmentServiceTest.kt
+++ src/test/kotlin/com/procurement/revision/application/service/AmendmentServiceTest.kt
@@ -304,8 +304,8 @@ internal class AmendmentServiceTest {
             whenever(generable.generateToken()).thenReturn(token)
             whenever(
                 amendmentRepository.saveNewAmendment(
-                    cpid = eq(params.cpid.value),
-                    ocid = eq(params.ocid.value),
+                    cpid = eq(params.cpid.toString()),
+                    ocid = eq(params.ocid.toString()),
                     amendment = any()
                 )
             ).thenReturn(Result.success(true))
@@ -328,8 +328,8 @@ internal class AmendmentServiceTest {
             whenever(generable.generateToken()).thenReturn(token)
             whenever(
                 amendmentRepository.saveNewAmendment(
-                    cpid = eq(params.cpid.value),
-                    ocid = eq(params.ocid.value),
+                    cpid = eq(params.cpid.toString()),
+                    ocid = eq(params.ocid.toString()),
                     amendment = any()
                 )
             ).thenReturn(Result.success(true))
@@ -370,14 +370,14 @@ internal class AmendmentServiceTest {
             whenever(generable.generateToken()).thenReturn(token)
             whenever(
                 amendmentRepository.saveNewAmendment(
-                    cpid = eq(params.cpid.value),
-                    ocid = eq(params.ocid.value),
+                    cpid = eq(params.cpid.toString()),
+                    ocid = eq(params.ocid.toString()),
                     amendment = any()
                 )
             ).thenReturn(Result.success(false))
 
             val amendmentFromDb = getTestAmendment()
-            whenever(amendmentRepository.findBy(params.cpid.value, params.ocid.value, params.amendment.id)).thenReturn(
+            whenever(amendmentRepository.findBy(params.cpid.toString(), params.ocid.toString(), params.amendment.id)).thenReturn(
                 Result.success(amendmentFromDb)
             )
 

4a247783aaf441704b2e825ff12734b6040a025f
katherine <kate.shaidurova@gmail.com>
2020-03-30 16:08:41 +0300

    ES-2733 Added InvalidToken, InvalidOwner, AmendmentNotFound validation errors

--- src/main/kotlin/com/procurement/revision/infrastructure/fail/error/ValidationError.kt
+++ src/main/kotlin/com/procurement/revision/infrastructure/fail/error/ValidationError.kt
@@ -10,4 +10,11 @@ sealed class ValidationError(numberError: String, override val description: Stri
         numberError = "1",
         description = "Document '${documentId}' has invalid documentType."
     )
+
+    class InvalidToken() : ValidationError(numberError = "10.2.4.1", description = "Request token doesn't match token from the database.")
+
+    class InvalidOwner() : ValidationError(numberError = "10.2.4.2", description = "Request owner doesn't match owner from the database.")
+
+    class AmendmentNotFound() : ValidationError(numberError = "10.2.4.3", description = "Amendment not found.")
+
 }

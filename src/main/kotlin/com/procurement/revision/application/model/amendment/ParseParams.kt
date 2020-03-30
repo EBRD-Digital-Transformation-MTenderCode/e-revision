4327288ea75267066b908b069b76f000029f47d2
katherine <kate.shaidurova@gmail.com>
2020-03-30 16:10:15 +0300

    ES-2733 Added parseAmendmentId, parseToken and parseOwner methods to ParseParams.kt

--- src/main/kotlin/com/procurement/revision/application/model/amendment/ParseParams.kt
+++ src/main/kotlin/com/procurement/revision/application/model/amendment/ParseParams.kt
@@ -4,6 +4,12 @@ import com.procurement.revision.domain.functional.Result
 import com.procurement.revision.domain.functional.asSuccess
 import com.procurement.revision.domain.model.Cpid
 import com.procurement.revision.domain.model.Ocid
+import com.procurement.revision.domain.model.Owner
+import com.procurement.revision.domain.model.Token
+import com.procurement.revision.domain.model.amendment.AmendmentId
+import com.procurement.revision.domain.model.amendment.tryAmendmentId
+import com.procurement.revision.domain.model.tryOwner
+import com.procurement.revision.domain.model.tryToken
 import com.procurement.revision.infrastructure.fail.error.DataErrors
 
 fun parseCpid(value: String): Result<Cpid, DataErrors.Validation.DataMismatchToPattern> =
@@ -27,3 +33,40 @@ fun parseOcid(value: String): Result<Ocid, DataErrors.Validation.DataMismatchToP
                 actualValue = value
             )
         )
+
+fun parseAmendmentId(value: String): Result<AmendmentId, DataErrors.Validation.DataFormatMismatch> =
+    value.tryAmendmentId()
+        .doReturn {
+            return Result.failure(
+                DataErrors.Validation.DataFormatMismatch(
+                    name = "amendmentId",
+                    expectedFormat = "uuid",
+                    actualValue = value
+                )
+            )
+        }.asSuccess()
+
+fun parseToken(value: String): Result<Token, DataErrors.Validation.DataFormatMismatch> =
+    value.tryToken()
+        .doReturn {
+            return Result.failure(
+                DataErrors.Validation.DataFormatMismatch(
+                    name = "token",
+                    expectedFormat = "uuid",
+                    actualValue = value
+                )
+            )
+        }.asSuccess()
+
+fun parseOwner(value: String): Result<Owner, DataErrors.Validation.DataFormatMismatch> =
+    value.tryOwner()
+        .doReturn {
+            return Result.failure(
+                DataErrors.Validation.DataFormatMismatch(
+                    name = "owner",
+                    expectedFormat = "uuid",
+                    actualValue = value
+                )
+            )
+        }.asSuccess()
+

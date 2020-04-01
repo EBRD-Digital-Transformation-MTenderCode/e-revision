492fe7ca357ff77e1795cbf6a687c770d33ca40e
katherine <kate.shaidurova@gmail.com>
2020-04-01 14:44:46 +0300

    ES-2778 Replaced string cpid and ocid with Cpid and Ocid types

--- src/main/kotlin/com/procurement/revision/application/repository/AmendmentRepository.kt
+++ src/main/kotlin/com/procurement/revision/application/repository/AmendmentRepository.kt
@@ -1,12 +1,14 @@
 package com.procurement.revision.application.repository
 
 import com.procurement.revision.domain.functional.Result
+import com.procurement.revision.domain.model.Cpid
+import com.procurement.revision.domain.model.Ocid
 import com.procurement.revision.domain.model.amendment.Amendment
 import com.procurement.revision.domain.model.amendment.AmendmentId
 import com.procurement.revision.infrastructure.fail.Fail
 
 interface AmendmentRepository {
-    fun findBy(cpid: String, ocid: String): Result<List<Amendment>, Fail.Incident>
-    fun findBy(cpid: String, ocid: String, id: AmendmentId): Result<Amendment?, Fail.Incident>
-    fun saveNewAmendment(cpid: String, ocid: String, amendment: Amendment): Result<Boolean, Fail.Incident>
+    fun findBy(cpid: Cpid, ocid: Ocid): Result<List<Amendment>, Fail.Incident>
+    fun findBy(cpid: Cpid, ocid: Ocid, id: AmendmentId): Result<Amendment?, Fail.Incident>
+    fun saveNewAmendment(cpid: Cpid, ocid: Ocid, amendment: Amendment): Result<Boolean, Fail.Incident>
 }

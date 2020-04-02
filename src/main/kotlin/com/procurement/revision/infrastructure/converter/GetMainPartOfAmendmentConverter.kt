53d3aa06839f07aed89b4cfd7b7fdee4ba074f14
katherine <kate.shaidurova@gmail.com>
2020-04-02 16:21:30 +0300

    ES-2808 Added GetMainPartOfAmendmentConverter

---
+++ src/main/kotlin/com/procurement/revision/infrastructure/converter/GetMainPartOfAmendmentConverter.kt
@@ -0,0 +1,23 @@
+package com.procurement.revision.infrastructure.converter
+
+import com.procurement.revision.application.model.amendment.state.GetMainPartOfAmendmentParams
+import com.procurement.revision.application.model.amendment.state.GetMainPartOfAmendmentResult
+import com.procurement.revision.domain.functional.Result
+import com.procurement.revision.domain.functional.Result.Companion.failure
+import com.procurement.revision.domain.model.amendment.Amendment
+import com.procurement.revision.infrastructure.fail.error.DataErrors
+import com.procurement.revision.infrastructure.web.dto.request.amendment.GetMainPartOfAmendmentRequest
+
+fun GetMainPartOfAmendmentRequest.convert(): Result<GetMainPartOfAmendmentParams, DataErrors> =
+    GetMainPartOfAmendmentParams.tryCreate(
+        cpid = cpid, ocid = ocid, amendmentIds = amendmentIds
+    ).doOnError { error -> return failure(error) }
+
+fun Amendment.convertToGetMainPartOfAmendmentResult() =
+    GetMainPartOfAmendmentResult(
+        id = id,
+        status = status,
+        relateditem = relatedItem,
+        relatesTo = relatesTo,
+        type = type
+    )

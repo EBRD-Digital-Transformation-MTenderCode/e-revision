c331c2876c4b029eae7faf3420023bf0fdb75fc5
katherine <kate.shaidurova@gmail.com>
2020-04-03 16:55:49 +0300

    ES-2808 Deleted redundant doOnError in GetMainPartOfAmendmentConverter

--- src/main/kotlin/com/procurement/revision/infrastructure/converter/GetMainPartOfAmendmentConverter.kt
+++ src/main/kotlin/com/procurement/revision/infrastructure/converter/GetMainPartOfAmendmentConverter.kt
@@ -3,7 +3,6 @@ package com.procurement.revision.infrastructure.converter
 import com.procurement.revision.application.model.amendment.part.GetMainPartOfAmendmentParams
 import com.procurement.revision.application.model.amendment.part.GetMainPartOfAmendmentResult
 import com.procurement.revision.domain.functional.Result
-import com.procurement.revision.domain.functional.Result.Companion.failure
 import com.procurement.revision.domain.model.amendment.Amendment
 import com.procurement.revision.infrastructure.fail.error.DataErrors
 import com.procurement.revision.infrastructure.web.dto.request.amendment.GetMainPartOfAmendmentRequest
@@ -11,7 +10,7 @@ import com.procurement.revision.infrastructure.web.dto.request.amendment.GetMain
 fun GetMainPartOfAmendmentRequest.convert(): Result<GetMainPartOfAmendmentParams, DataErrors> =
     GetMainPartOfAmendmentParams.tryCreate(
         cpid = cpid, ocid = ocid, amendmentIds = amendmentIds
-    ).doOnError { error -> return failure(error) }
+    )
 
 fun Amendment.convertToGetMainPartOfAmendmentResult() =
     GetMainPartOfAmendmentResult(
